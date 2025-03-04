e87f1a7f7f7a8d9bd683b29a4f348cad96523f12
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2025-03-04 02:17:24 +0100

    Added CANINE base model.

--- src/archit/instantiation/basemodels.py
+++ src/archit/instantiation/basemodels.py
@@ -141,3 +141,49 @@ class DebertaBaseModel(BaseModel[DebertaConfig]):
             num_attention_heads=raw_config.num_attention_heads,
             context_length=raw_config.max_position_embeddings or (2*raw_config.max_relative_positions-1)*raw_config.num_hidden_layers  # Explanation for this formula: https://bauwenst.github.io/posts/explainers/2024-09-22-DeBERTa-receptive-field/
         )
+
+
+from transformers.models.canine.modeling_canine import CanineModel, CanineConfig
+
+class CanineBaseModel(BaseModel[CanineConfig]):
+
+    def forward(
+        self,
+        input_ids: Tensor,
+        attention_mask: Tensor,
+        do_drop_intermediates: bool=True,
+        **kwargs
+    ) -> AllHiddenStatesAndPooling:
+        output: BaseModelOutput = self.base_model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            return_dict=True,
+            output_hidden_states=not do_drop_intermediates,
+            **kwargs
+        )
+        return AllHiddenStatesAndPooling(
+            last_hidden_state=output.last_hidden_state,
+            hidden_states=output.hidden_states,
+            pooler_output=None
+        )
+
+    @classmethod
+    @property
+    def config_class(cls):
+        return CanineConfig
+
+    @classmethod
+    def buildCore(cls, raw_config):
+        return CanineModel(raw_config)
+
+    @classmethod
+    def standardiseConfig(cls, raw_config: CanineConfig) -> BaseModelConfig:
+        return BaseModelConfig(
+            hidden_size=raw_config.hidden_size,
+            hidden_dropout_prob=raw_config.hidden_dropout_prob,
+            vocab_size=None,  # Could also use len(CanineTokenizer().get_vocab()) but that's equal to over 1 million.
+
+            num_hidden_layers=raw_config.num_hidden_layers,
+            num_attention_heads=raw_config.num_attention_heads,
+            context_length=raw_config.max_position_embeddings
+        )
